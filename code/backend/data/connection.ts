4c148b973caa594b3cb3d1f2d77dae4d027355fe
Aureliu555 <A48285@alunos.isel.pt>
2024-08-06 17:55:59 +0100

    backend now connected to deployed db + students page layout done

--- code/backend/data/connection.ts
+++ code/backend/data/connection.ts
@@ -2,21 +2,16 @@ import { Pool } from "pg"
 import dotenv from "dotenv"
 dotenv.config()
 
-const dbPort = process.env.DB_PORT ? parseInt(process.env.DB_PORT) : undefined 
-const dbMaxConnections = process.env.DB_MAX_CONNECTIONS ? parseInt(process.env.DB_MAX_CONNECTIONS) : undefined
-const dbIdleTimeoutMillis = process.env.DB_IDLE_TIMEOUT_MS ? parseInt(process.env.DB_IDLE_TIMEOUT_MS) : undefined
-const dbConnectionTimeoutMillis = process.env.DB_CONNECTION_TIMEOUT_MS ? parseInt(process.env.DB_CONNECTION_TIMEOUT_MS) : undefined
-
+// if the localhost database does not accept ssl connection, set (ssl: false) when using local connection
 export default function getPool(): Pool {
     return new Pool({
-        user: process.env.DB_USER,
-        host: process.env.DB_HOST,
-        database: process.env.DB_NAME,
-        password: process.env.DB_PASSWORD,
-        port: dbPort,
-        max: dbMaxConnections,
-        idleTimeoutMillis: dbIdleTimeoutMillis,
-        connectionTimeoutMillis: dbConnectionTimeoutMillis
+        connectionString: process.env.DB_URL, 
+        ssl: {
+            rejectUnauthorized: false 
+        },
+        max: 20,
+        idleTimeoutMillis: 30000,
+        connectionTimeoutMillis: 2000
     })
 }
 
--- code/frontend/src/pages/Students.jsx
+++ code/frontend/src/pages/Students.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react"
 import { getToken } from "../services/storage"
 import { getStudents } from "../services/students"
 import Loading from "../components/common/Loading"
+import "../styles/students/Students.css"
+import bin_icon from "../assets/icons/bin.png"
 
 export default function Students() {
     const [students, setStudents] = useState(null)
@@ -16,10 +18,35 @@ export default function Students() {
 
     return (
         <>
-            { !students ? <Loading /> :
-                <div>Students</div>
-            }
+            { !students 
+                ? <Loading />
+                : <div className="students_container">
+                    <Table students={students} />
+                </div>
+            }   
         </>
+    )
+}
+
+function Table({students}) {
+    return (
+        <div className="table_container"> 
+            <div className="table_name"> Students </div>
+            {students.map((s, idx) => 
+                <TableRow key={s.id} number={idx+1} name={s.name} />
+            )}
+            <div className="add_student_button">ADD NEW STUDENT<span>+</span></div>
+         </div>
+    )
+}
+
+function TableRow({key, number, name}) {
+    return (
+        <div key={key} className="table_row">
+            <div className="row_number">{number}.</div>
+            <div className="row_student_name"><span>{name}</span></div>
+            <div className="row_delete_icon"> <img className="delete_icon" src={bin_icon} alt="bin_icon" /></div>
+        </div>
     )
 }
 
